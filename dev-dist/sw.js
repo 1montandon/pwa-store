869c1795d99f17bf1e534ac08c1177f14f246e0e
Eduardo Montandon <emontandon11@gmail.com>
2024-02-27 15:14:04 -0300

    Logo change

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lnq4th3f2ho"
+    "revision": "0.2u90le454ug"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
